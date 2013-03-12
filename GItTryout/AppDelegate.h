8dbd7540f22f140bb0e30e74b041e6120892d104
Amit Lesman <amitlesman@Amits-MacBook-Pro.local>
2013-03-12 17:19:30 +0200

    Initial Commit

---
+++ GItTryout/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GItTryout
+//
+//  Created by Amit Lesman on 12/3/13.
+//  Copyright (c) 2013 Amit Lesman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
